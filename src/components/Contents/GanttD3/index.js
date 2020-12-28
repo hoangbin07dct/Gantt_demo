157a7069019fd698f5c159f9df7d427384518dca
vuongnguyenvu <vuvn@caters.co.jp>
2020-12-28 15:22:45 +0700

    revert index

--- src/components/Contents/GanttD3/index.js
+++ src/components/Contents/GanttD3/index.js
@@ -8,88 +8,70 @@ const getWidth = () => {
 const GanttD3 = (props) => {
   var initData = [
     {
-      id: 1,
       task: 'conceptualize',
       type: 'development',
       startTime: '2013-1-28', //year/month/day
       endTime: '2013-2-1',
       details: "This actually didn't take any conceptualization",
-      progress: 90
     },
 
     {
-      id: 2,
       task: 'sketch',
       type: 'development',
       startTime: '2013-2-1',
       endTime: '2013-2-6',
       details: 'No sketching either, really',
-      progress: 20
     },
 
     {
-      id: 3,
       task: 'color profiles',
       type: 'development',
       startTime: '2013-2-6',
       endTime: '2013-2-9',
-      progress: 45
     },
 
     {
-      id: 4,
       task: 'HTML',
       type: 'coding',
       startTime: '2013-2-2',
       endTime: '2013-2-6',
       details: 'all three lines of it',
-      progress: 25
     },
 
     {
-      id: 5,
       task: 'write the JS',
       type: 'coding',
       startTime: '2013-2-6',
       endTime: '2013-2-9',
-      progress: 60
     },
 
     {
-      id: 6,
       task: 'advertise',
       type: 'promotion',
       startTime: '2013-2-9',
       endTime: '2013-2-12',
       details: 'This counts, right?',
-      progress: 75
     },
 
     {
-      id: 7,
       task: 'spam links',
       type: 'promotion',
       startTime: '2013-2-12',
       endTime: '2013-2-14',
-      progress: 100
     },
     {
-      id: 8,
       task: 'eat',
       type: 'celebration',
       startTime: '2013-2-8',
       endTime: '2013-2-13',
       details: 'All the things',
-      progress: 50
     },
 
     {
-      id: 9,
       task: 'crying',
       type: 'celebration',
       startTime: '2013-2-13',
       endTime: '2013-2-16',
-      progress: 20
     },
   ];
   const [width, setWidth] = useState(getWidth());
