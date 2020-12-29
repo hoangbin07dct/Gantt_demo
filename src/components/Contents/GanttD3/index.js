d0d38de66ffddc3abe892ffa1b0a866d3982fec2
tranhoan97 <hoanvt@caters.co.jp>
2020-12-29 08:20:03 +0700

    [update] Add Gantt Table

--- src/components/Contents/GanttD3/index.js
+++ src/components/Contents/GanttD3/index.js
@@ -1,114 +1,108 @@
-import React, {
-  useCallback,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
-import _, { debounce } from "lodash";
-import GanttChart from "./ganttChart";
-import withRouter from "./index";
-import GanttTable from "./GanttTable";
-import common from "../../../styles/Common.module.scss";
-import Modal from "./Modal";
-import useModal from "./useModal";
-import moment from "moment";
-import Datetime from "react-datetime";
-import "react-datetime/css/react-datetime.css";
+import React, { useCallback, useContext, useEffect, useRef, useState } from 'react';
+import _, { debounce } from 'lodash';
+import GanttChart from './ganttChart';
+import withRouter from './index';
+import GanttTable from './GanttTable';
+import common from '../../../styles/Common.module.scss';
+import Modal from './Modal';
+import useModal from './useModal';
+import moment from 'moment';
+import Datetime from 'react-datetime';
+import 'react-datetime/css/react-datetime.css';
 
 const GanttD3 = (props) => {
   const getWidth = () => {
-    return document.getElementById("dom").getBoundingClientRect().width;
+    return document.getElementById('dom').getBoundingClientRect().width;
   };
   var initData = [
     {
       id: 1,
-      task: "conceptualize",
-      type: "development",
-      startTime: "2020-12-01", //year/month/day
-      endTime: "2020-12-02",
+      task: 'conceptualize',
+      type: 'development',
+      startTime: '2020-12-01', //year/month/day
+      endTime: '2020-12-02',
       details: "This actually didn't take any conceptualization",
       progress: 90,
     },
 
     {
       id: 2,
-      task: "sketch",
-      type: "development",
-      startTime: "2020-12-02", //year/month/day
-      endTime: "2020-12-05",
-      details: "No sketching either, really",
+      task: 'sketch',
+      type: 'development',
+      startTime: '2020-12-02', //year/month/day
+      endTime: '2020-12-05',
+      details: 'No sketching either, really',
       progress: 20,
     },
 
     {
       id: 3,
-      task: "color profiles",
-      type: "development",
-      startTime: "2020-12-05", //year/month/day
-      endTime: "2020-12-10",
+      task: 'color profiles',
+      type: 'development',
+      startTime: '2020-12-05', //year/month/day
+      endTime: '2020-12-10',
       progress: 45,
     },
 
     {
       id: 4,
-      task: "HTML",
-      type: "coding",
-      startTime: "2020-12-04", //year/month/day
-      endTime: "2020-12-09",
-      details: "all three lines of it",
+      task: 'HTML',
+      type: 'coding',
+      startTime: '2020-12-04', //year/month/day
+      endTime: '2020-12-09',
+      details: 'all three lines of it',
       progress: 25,
     },
 
     {
       id: 5,
-      task: "write the JS",
-      type: "coding",
-      startTime: "2020-12-11", //year/month/day
-      endTime: "2020-12-20",
+      task: 'write the JS',
+      type: 'coding',
+      startTime: '2020-12-11', //year/month/day
+      endTime: '2020-12-20',
       progress: 60,
     },
 
     {
       id: 6,
-      task: "advertise",
-      type: "promotion",
-      startTime: "2020-12-15", //year/month/day
-      endTime: "2020-12-18",
-      details: "This counts, right?",
+      task: 'advertise',
+      type: 'promotion',
+      startTime: '2020-12-15', //year/month/day
+      endTime: '2020-12-18',
+      details: 'This counts, right?',
       progress: 75,
     },
 
     {
       id: 7,
-      task: "spam links",
-      type: "promotion",
-      startTime: "2020-12-17", //year/month/day
-      endTime: "2020-12-25",
+      task: 'spam links',
+      type: 'promotion',
+      startTime: '2020-12-17', //year/month/day
+      endTime: '2020-12-25',
       progress: 100,
     },
     {
       id: 8,
-      task: "eat",
-      type: "celebration",
-      startTime: "2020-12-25", //year/month/day
-      endTime: "2020-12-27",
-      details: "All the things",
+      task: 'eat',
+      type: 'celebration',
+      startTime: '2020-12-25', //year/month/day
+      endTime: '2020-12-27',
+      details: 'All the things',
       progress: 50,
     },
 
     {
       id: 9,
-      task: "crying",
-      type: "celebration",
-      startTime: "2020-12-24", //year/month/day
-      endTime: "2020-12-27",
+      task: 'crying',
+      type: 'celebration',
+      startTime: '2020-12-24', //year/month/day
+      endTime: '2020-12-27',
       progress: 20,
     },
   ];
   const [width, setWidth] = useState();
   const [data, setData] = useState(initData);
-  const [from, setFrom] = useState(moment().subtract(30, "days"));
+  const [from, setFrom] = useState(moment().subtract(30, 'days'));
   const [to, setTo] = useState(moment());
   const chartRef = useRef();
   const ganttChart = useRef();
@@ -122,65 +116,47 @@ const GanttD3 = (props) => {
 
   useEffect(() => {
     if (chartRef.current) {
-      chartRef.current.innerHTML = "";
+      chartRef.current.innerHTML = '';
     }
-    ganttChart.current = new GanttChart(
-      chartRef.current,
-      width,
-      window.innerHeight / 1.9,
-      from,
-      to
-    );
+    ganttChart.current = new GanttChart(chartRef.current, width, window.innerHeight / 1.9, from, to);
     ganttChart.current.render(data);
   }, [width]);
   useEffect(() => {
     // ganttChart.current.changeScale(from, to);
     if (chartRef.current) {
-      chartRef.current.innerHTML = "";
+      chartRef.current.innerHTML = '';
     }
-    ganttChart.current = new GanttChart(
-      chartRef.current,
-      width,
-      window.innerHeight / 1.9,
-      from,
-      to
-    );
+    ganttChart.current = new GanttChart(chartRef.current, width, window.innerHeight / 1.9, from, to);
     ganttChart.current.render(data);
-  }, [from, to])
+  }, [from, to]);
 
   useEffect(() => {
     setData(initData);
     setWidth(getWidth());
 
-    window.addEventListener("resize", resizeListener);
+    window.addEventListener('resize', resizeListener);
     return () => {
-      window.removeEventListener("resize", resizeListener);
+      window.removeEventListener('resize', resizeListener);
     };
   }, []);
 
   const [infoForm, setInfoForm] = useState({
-    task: "",
-    type: "",
-    startTime: "",
-    endTime: "",
-    details: "",
+    task: '',
+    type: '',
+    startTime: '',
+    endTime: '',
+    details: '',
   });
 
-  const [value, setValue] = React.useState("");
+  const [value, setValue] = React.useState('');
   const addTodo = (text) => {};
 
   const handleSubmit = (e) => {
     e.preventDefault();
     initData.push(infoForm);
     console.log(initData);
-    document.querySelector("#dom svg").remove();
-    const ganttChart = new GanttChart(
-      chartRef.current,
-      width,
-      window.innerHeight / 1.9,
-      from,
-      to
-    );
+    document.querySelector('#dom svg').remove();
+    const ganttChart = new GanttChart(chartRef.current, width, window.innerHeight / 1.9, from, to);
     ganttChart.render(initData);
   };
 
@@ -193,10 +169,10 @@ const GanttD3 = (props) => {
 
   const updateFrom = (e) => {
     setFrom(e);
-  }
+  };
   const updateTo = (e) => {
     setTo(e);
-  }
+  };
 
   return (
     <React.Fragment>
@@ -222,18 +198,24 @@ const GanttD3 = (props) => {
         />
       </div>
       <div className={common.container}>
-        <button className={common.button_default} onClick={toggle}>
-          Add New Task
-        </button>
-        <Modal
-          isShowing={isShowing}
-          hide={toggle}
-          handleSubmit={handleSubmit}
-          InputChange={InputChange}
-          infoForm={infoForm}
-        />
+        <div className={common.tableWrapper}>
+          <ul className={common.btnList}>
+            <li>
+              <button className={common.button_default} onClick={toggle}>
+                Add New Task
+              </button>
+            </li>
+          </ul>
+          <Modal
+            isShowing={isShowing}
+            hide={toggle}
+            handleSubmit={handleSubmit}
+            InputChange={InputChange}
+            infoForm={infoForm}
+          />
 
-        <GanttTable data={data}></GanttTable>
+          <GanttTable data={data}></GanttTable>
+        </div>
         <div id="dom" className={common.dom} ref={chartRef}></div>
       </div>
     </React.Fragment>
