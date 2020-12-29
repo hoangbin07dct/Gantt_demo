841cdd1c9bd5e944d3bff54c72a465ed962da3e8
tranhoan97 <hoanvt@caters.co.jp>
2020-12-29 13:22:48 +0700

    [update] Add Gantt Tableedit dataedit level data

--- src/components/Contents/GanttD3/GanttTable.js
+++ src/components/Contents/GanttD3/GanttTable.js
@@ -1,25 +1,36 @@
 import React, { useEffect } from 'react';
-import common from "../../../styles/Common.module.scss";
-
+import common from '../../../styles/Common.module.scss';
 
 const GanttTable = ({ ...props }) => {
   useEffect(() => {
     console.log(props);
   }, []);
 
+  let group = props.data;
+
+  let currentGroup = '';
+
   return (
     <table className={common.taskTable}>
       <thead>
         <tr>
+          <th>Group</th>
           <th>Task Name</th>
           <th>Progress</th>
         </tr>
       </thead>
       <tbody>
         {props.data.map((element, key) => {
+          let td;
+
+          if (element.type !== currentGroup) {
+            currentGroup = element.type;
+            td = <td className={common.vertAlign} rowSpan={props.data.filter((el) => el.type === currentGroup).length}>{element.type}</td>;
+          }
           return (
             <tr key={key}>
-              <td>{element.task}</td>
+              {td}
+              <td style={{ paddingLeft: (element.level - 1) * 24 + 5 + 'px' }}>{element.task}</td>
               <td>{element.progress}%</td>
             </tr>
           );
