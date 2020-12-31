18e60b9f18129eca807f3e2957097f9eefce24db
tranhoan97 <hoanvt@caters.co.jp>
2020-12-31 15:19:31 +0700

    [update] header style

--- src/components/Header/Index.js
+++ src/components/Header/Index.js
@@ -3,14 +3,14 @@ import common from '../../styles/Common.module.scss';
 
 const Header = () => {
   return (
-    <React.Fragment>
-      <header className={common.header}>
-        <h1 className={common.header_text}>建設マネジメントシステム</h1>
+      <header>
+        <div className={common.header}>
+          <h1 className={common.header_text}>建設マネジメントシステム</h1>
+        </div>
+        <div className={common.management}>
+          <h2 className={common.management_text}>システム 開発者 (カテル有限会社)</h2>
+        </div>
       </header>
-      <div className={common.management}>
-        <h2 className={common.management_text}>システム 開発者 (カテル有限会社)</h2>
-      </div>
-    </React.Fragment>
   );
 };
 
